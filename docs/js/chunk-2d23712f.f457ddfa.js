6c65c2d32306bf9eb98a09c7b8aa51f3a8319ab9
chakulama <chaku.lama@gmail.com>
2023-03-12 22:19:33 +0000

    PIPELINE - deploying build static web site

--- docs/js/chunk-2d23712f.f457ddfa.js
+++ docs/js/chunk-2d23712f.f457ddfa.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d23712f"],{fa20:function(n,c,e){"use strict";e.r(c);var s=e("7a23");const t=["src"];function r(n,c){return Object(s["t"])(),Object(s["e"])("img",{class:"image-section",src:e("2064")},null,8,t)}var a=e("6b0d"),o=e.n(a);const i={},u=o()(i,[["render",r]]);c["default"]=u}}]);
-//# sourceMappingURL=chunk-2d23712f.7cc1bf21.js.map
+//# sourceMappingURL=chunk-2d23712f.f457ddfa.js.map
